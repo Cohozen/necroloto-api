4af8d1783128794c8879e315296596cfd74eafef
Cohozen <corentin.legal@protonmail.com>
2024-08-18 22:00:50 +0200

    feat: Add Swagger API documentation setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, {
+        logger: ["debug", "log", "verbose", "error", "fatal", "warn"]
+    });
+
+    const config = new DocumentBuilder()
+        .setTitle("Necroloto API")
+        .setDescription("The necroloto API description")
+        .setVersion("0.1")
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("api", app, document);
+
     await app.listen(3000);
 }
 
